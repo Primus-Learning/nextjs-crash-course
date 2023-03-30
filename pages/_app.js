de111a708304a4566def65617b57cbc493e4ad86
Joseph David <techiejossy@gmail.com>
2023-03-30 04:01:15 +0100

    starter files

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,5 @@
-import { MantineProvider } from "@mantine/core";
-import { SessionProvider } from "next-auth/react";
-import "node_modules/react-modal-video/scss/modal-video.scss";
-import { Notifications } from "@mantine/notifications";
-import Layout from "../components/layout";
-import RouterTransition from "@/components/RouterTransition";
-
-function MyApp({ Component, pageProps: { session, ...pageProps } }) {
-  return (
-    <MantineProvider
-      withGlobalStyles
-      withNormalizeCSS
-      theme={{
-        colorScheme: "dark",
-      }}
-    >
-      <RouterTransition />
-      <Notifications />
-      <SessionProvider session={session}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </SessionProvider>
-    </MantineProvider>
-  );
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,5 @@
-import AuthenticatedScreen from "@/components/AuthenticatedScreen";
-import UnaunthenticatedScreen from "@/components/UnauthenticatedScreen";
-import { useSession } from "next-auth/react";
-
 function Home() {
-  const { data: session } = useSession();
-
-  if (session) {
-    return <AuthenticatedScreen />;
-  }
-  return <UnaunthenticatedScreen />;
+  return <h1>Welcome to ReelRanger</h1>;
 }
 
 export default Home;
-
